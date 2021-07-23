c18bd430678c19840fd031fb4ba6d5b1ec28276f
Jesus Lara <jelara99@gmail.com>
2021-07-22 23:09:43 -0600

    modal loggin completo

---
+++ components/loginModal.js
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import { Button } from "react-bootstrap";
+
+export default function LoginModal() {
+    const [login, setlogin] = useState(false);
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const loginShow = () => setlogin(true);
+    const loginClose = () => setlogin(false);
+    const validateForm = () => {
+        return email.length > 0 && password.length > 0;
+    };
+    const validarUser = (user, password) => {
+        if (email && password !== "") {
+            alert("bienvenido");
+            loginClose();
+        } else {
+            alert("set user and password");
+        }
+    };
+    return (
+        <>
+            <a className="nav-link " aria-current="page" onClick={() => loginShow()}><i className="fas fa-user"></i>{" "}Iniciar sesión</a>
+            <Modal show={login} onHide={loginClose} backdrop="static" keyboard={false} animation={false}>
+                <Modal.Header>
+                    <button type="button" className="btn-close" onClick={loginClose} aria-label="Close"></button></Modal.Header>
+                <Modal.Body>
+                    <Modal.Title>¡Bienvenido!</Modal.Title>
+                    <Form className="text-center mt-5">
+                        <Form.Group size="lg" controlId="email">
+                            <Form.Control
+                                className="credenciales mx-auto"
+                                placeholder="Correo electrónico"
+                                autoFocus
+                                type="email"
+                                autoComplete="email"
+                                value={email}
+                                onChange={e => setEmail(e.target.value)}
+                                onKeyPress={e => {
+                                    if (e.key == "Enter") {
+                                        validarUser(email, password);
+                                    }
+                                }}
+                            />
+                        </Form.Group>
+                        <Form.Group size="lg" controlId="password" className="mt-2" >
+                            <Form.Control
+                                className="credenciales mx-auto"
+                                placeholder="Contraseña"
+                                type="password"
+                                autoComplete="password"
+                                value={password}
+                                onChange={e => setPassword(e.target.value)}
+                                onKeyPress={e => {
+                                    if (e.key == "Enter") {
+                                        validarUser(email, password);
+                                    }
+                                }}
+                            />
+                        </Form.Group>
+                        <Button
+                            className="mt-5 btEntrar"
+                            variant="success"
+                            type="submit"
+                            disabled={!validateForm()}
+                            onClick={() => validarUser(email, password)}>
+                            Entrar
+                        </Button>
+                        <Form.Group size="lg" className="mt-5 text-center">
+                            <p className="linea"><span>O inicia sesion con tu red</span></p>
+                            <div className="container mt-2">
+                                <button type="button" className="btn btnSocial pr-3"><img src="/google.png" className="redSocial" alt="google" />{" "}Google</button>
+                                <button type="button" className="btn btnSocial pl-3"><img src="/facebook.png" className="redSocial" alt="facebook" />{" "}Facebook</button>
+                            </div>
+                        </Form.Group>
+                    </Form>
+                </Modal.Body>
+                <Modal.Footer className="mt-3">
+                    <div className="mx-auto"><p>Aun no tienes cuenta?, <a className="text-decoration-none" href="#">registrate</a> ahora!</p></div>
+                </Modal.Footer>
+            </Modal>
+        </>
+    )
+}
--- components/navbar.js
+++ components/navbar.js
@@ -1,126 +1,48 @@
 import Link from "next/link";
-import { useState } from "react";
-import Modal from "react-bootstrap/Modal";
-import Form from "react-bootstrap/Form";
-import { Button } from "react-bootstrap";
+import LoginModal from "../components/loginModal"
 
 export default function Navbar() {
-    return (
-        <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-            <div className="container">
-
-                <Link href="/">
-                    <a className="navbar-brand">
-                        <img src="/logo.png" alt="distribuidora diamasa" className="logo" />
-                    </a>
-                </Link>
-
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav ">
-                        <li className="nav-item">
-                            <Cuenta />
-                        </li>
-                        <li className="nav-item">
-                            <Link href="/rutas">
-                                <a className="nav-link" aria-current="page" >Entregas</a>
-                            </Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link href="/info">
-                                <a className="nav-link" >¿Quienes Somos?</a>
-                            </Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            <div className="col-12 col-lg-4">
-                <div className="row justify-content-center">
-                    <div className="col-10">
-                        <form className="d-flex ">
-                            <input className="form-control text-center me-2" type="search" placeholder="Buscar producto" aria-label="Search" />
-                            <button className="btn btn-light botonBusqueda" type="submit"><i className="fas fa-search"></i></button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    )
+	return (
+		<nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+			<div className="container">
+				<Link href="/">
+					<a className="navbar-brand">
+						<img src="/logo.png" alt="distribuidora diamasa" className="logo" />
+					</a>
+				</Link>
+				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+					<span className="navbar-toggler-icon"></span>
+				</button>
+				<div className="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul className="navbar-nav ">
+						<li className="nav-item">
+							<LoginModal />
+						</li>
+						<li className="nav-item">
+							<Link href="/rutas">
+								<a className="nav-link" aria-current="page" >Entregas</a>
+							</Link>
+						</li>
+						<li className="nav-item">
+							<Link href="/info">
+								<a className="nav-link" >¿Quienes Somos?</a>
+							</Link>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div className="col-12 col-lg-4">
+				<div className="row justify-content-center">
+					<div className="col-10">
+						<form className="d-flex ">
+							<input className="form-control text-center me-2" type="search" placeholder="Buscar producto" aria-label="Search" />
+							<button className="btn btn-light botonBusqueda" type="submit"><i className="fas fa-search"></i></button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</nav>
+	)
 }
 
-const Cuenta = () => {
-    const [login, setlogin] = useState(false);
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const loginShow = () => setlogin(true);
-    const loginClose = () => setlogin(false);
-    const validateForm = () => {
-		return email.length > 0 && password.length > 0;
-	};
-    const validarUser = (user, password) => {
-		if (email && password !== "") {
-			alert("bienvenido");
-            loginClose();
-		} else {
-			alert("set user and password");
-		}
-	};
-    return (
-        <>
-        <a className="nav-link " aria-current="page" onClick={() => loginShow()}><i className="fas fa-user"></i>{" "}Iniciar sesión</a>
-        <Modal className="modalNav" show={login} onHide={loginClose} backdrop="static" keyboard={false} animation={false}>
-						<Modal.Header className="part">
-							<Modal.Title>¡Bienvenido!</Modal.Title>
-						</Modal.Header>
-						<Modal.Body className="part">
-							<Form className="mt-3">
-								<Form.Group size="lg" controlId="email">
-									<Form.Label>Correo electrónico</Form.Label>
-									<Form.Control
-										autoFocus
-										type="email"
-										autoComplete="email"
-										value={email}
-										onChange={e => setEmail(e.target.value)}
-										onKeyPress={e => {
-											if (e.key == "Enter") {
-												validarUser(email, password);
-											}
-										}}
-									/>
-								</Form.Group>
-								<Form.Group size="lg" controlId="password" className="mt-2">
-									<Form.Label>Contraseña</Form.Label>
-									<Form.Control
-										type="password"
-										autoComplete="password"
-										value={password}
-										onChange={e => setPassword(e.target.value)}
-										onKeyPress={e => {
-											if (e.key == "Enter") {
-												validarUser(email, password);
-											}
-										}}
-									/>
-								</Form.Group>
-							</Form>
-						</Modal.Body>
-						<Modal.Footer className="part">
-							<Button variant="btn-warning" className="btn-warning" onClick={loginClose}>
-								Close
-							</Button>
-							<Button
-								variant="btn-warning"
-								className="btn-warning"
-								type="submit"
-								disabled={!validateForm()}
-								onClick={() => validarUser(email, password)}>
-								Get in
-							</Button>
-						</Modal.Footer>
-					</Modal>
-            </>
-    )
-}
+
