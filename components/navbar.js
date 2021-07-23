02b0907ba04ce4c91ab8ecdb5354267c8cbfa67d
GsusLara <jelara99@gmail.com>
2021-07-23 11:42:17 -0600

    estilos navbar

--- components/navbar.js
+++ components/navbar.js
@@ -3,8 +3,8 @@ import LoginModal from "../components/loginModal"
 
 export default function Navbar() {
 	return (
-		<nav className="navbar fixed-top navbar-expand-lg navbar-light bg-light">
-			<div className="container">
+		<nav className="navbar fixed-top navbar-expand-lg navbar-light">
+			<div className="container" >
 				<Link href="/">
 					<a className="navbar-brand">
 						<img src="/logo.png" alt="distribuidora diamasa" className="logo" />
@@ -13,7 +13,7 @@ export default function Navbar() {
 				<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span className="navbar-toggler-icon"></span>
 				</button>
-				<div className="collapse navbar-collapse" id="navbarSupportedContent">
+				<div className="collapse navbar-collapse text-center" id="navbarSupportedContent">
 					<ul className="navbar-nav ">
 						<li className="nav-item">
 							<LoginModal />
